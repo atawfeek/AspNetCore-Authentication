427f8f06fead4b48e26c0192cddf2e3b7511a003
Abdelrahman Tawfeek <Abdelrahman@ireckonu.com>
2020-10-16 16:42:34 +0200

    use options pattern to access strongly typed configurations.

---
+++ src/Api/Models/AuthOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCore.Authentication.Web.Models
+{
+    public class AuthOptions
+    {
+        public const string Auth = "Auth";
+
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SymmetricSecurityKey { get; set; }
+    }
+}
--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AspNetCore.Authentication.Web.Extensions;
+using AspNetCore.Authentication.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AspNetCore.Authentication.Web
 {
@@ -25,6 +20,10 @@ namespace AspNetCore.Authentication.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<AuthOptions>(Configuration.GetSection(
+                                        AuthOptions.Auth));
+
+            services.AddJwtAuthentication();
             services.AddControllers();
         }
 
@@ -36,10 +35,11 @@ namespace AspNetCore.Authentication.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection(); //adds middleware for redirecting HTTP requests to HTTPS.
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
